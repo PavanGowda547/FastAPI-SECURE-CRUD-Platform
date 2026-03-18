2056ec4ce26b3921600841c3b30fef0735202ea5
Asus <newg91945@gmail.com>
2026-03-18 22:56:35 +0530

    Phase 8 : Deploying Secure Notes using Render

--- database.py
+++ database.py
@@ -1,9 +1,27 @@
+from dotenv import load_dotenv
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./secure_notes.db"
+load_dotenv()  # loads .env file
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread":False})
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./secure_notes.db")
+
+# Fix postgres:// issue
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        DATABASE_URL,
+        connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(
+        DATABASE_URL,
+        connect_args={"sslmode": "require"}
+    )
+
 session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
